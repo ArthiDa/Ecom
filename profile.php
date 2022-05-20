df6380ba30a9778dba7130d3ff6c012db3fabc80
unknown <ayt.ullah.dev@gmail.com>
2022-05-20 17:42:29 +0600

    User information section added

--- profile.php
+++ profile.php
@@ -1,3 +1,40 @@
 <?php include('MenuXFooter/main.php') ?>
-
+<main>
+    <div class="main-banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <img src="https://picsum.photos/id/237/300/300" class="rounded-circle img-thumbnail" alt="">
+                </div>
+                <div class="col-lg-6 col-12">
+                    <h1 class="text-uppercase text-center py-5">User Information</h1>
+                    <table class="table">
+                        <thead>
+                            
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Name</th>
+                                <td>AYAT ULLAH</td>
+                                
+                            </tr>
+                            <tr>
+                                
+                                <td scope="row">Email</td>
+                                <td>ayatullah.dev@gmail.com</td>
+                                
+                            </tr>
+                            <tr>
+                                
+                                <td scope="row">Adress</td>
+                                <td>Adress here</td>
+                                
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 <?php include('MenuXFooter/footer.php') ?>
