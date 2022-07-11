bec58947ddb3815c279ca5ee529da43f3cf5f1c6
ArthiDa <arthida150@gmail.com>
2022-07-12 00:48:34 +0600

    triggerDone

--- admin/addProduct.php
+++ admin/addProduct.php
@@ -3,7 +3,7 @@
 <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Orders</h1>
+      <h1>Add New Products</h1>
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="<?php echo SITEURL;?>">Home</a></li>
